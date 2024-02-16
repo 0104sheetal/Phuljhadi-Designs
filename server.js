8e92ebfc3d79039448046974f0bc93f526ea9418
0104sheetal <sheetal.sharma@messold.com>
2024-02-16 06:47:37 +0530

    Server File

--- server.js
+++ server.js
@@ -60,6 +60,10 @@ app.get('/shopify/callback', async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
